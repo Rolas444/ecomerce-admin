a1884a0761cfd67a42f8170cdf209d4078d5924f
DESKTOP-PR8MKLN\hrodland <7200ws@gmail.com>
2023-05-22 18:12:07 -0500

    security routes, products and categories

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -16,6 +16,7 @@ const ProductForm = ({
     const [title, setTitle] = useState(existingTitle || "");
     const [description, setDescription] = useState(existingDescription || "");
     const [category, setCategory] = useState(assignedCategory || "");
+    const [productProperties, setProductProperties] = useState({});
     const [price, setPrice] = useState(existingPrice || "");
     const [images, setImages] = useState(existingImages || []);
     const [goToProducts, setGoToProducts] = useState(false);
@@ -24,14 +25,17 @@ const ProductForm = ({
     const router = useRouter();
 
     useEffect(() => {
-        axios.get('/api/categories').then(result=>{
+        axios.get('/api/categories').then(result => {
             setCategories(result.data)
         })
-    },[])
+    }, [])
 
     const saveProduct = async (ev) => {
         ev.preventDefault();
-        const data = { title, description, price, images, category };
+        const data = {
+            title, description, price, images, category,
+            properties: productProperties
+        };
         if (_id) {
             //update
             await axios.put('/api/products', { ...data, _id });
@@ -48,25 +52,46 @@ const ProductForm = ({
         router.push("/products");
     }
 
-    const uploadImages = async (ev)=>{
+    const uploadImages = async (ev) => {
         const files = ev.target?.files;
-        if(files?.length>0){
+        if (files?.length > 0) {
             setIsUploading(true);
-            const data= new FormData();
-            for (const file of files){
-                data.append('file',file);
+            const data = new FormData();
+            for (const file of files) {
+                data.append('file', file);
             }
             const res = await axios.post('/api/upload', data);
-            setImages(oldImages =>{
+            setImages(oldImages => {
                 return [...oldImages, ...res.data.links]
             });
             setIsUploading(false);
         }
 
     }
-    const updateImagesOrder = async (images)=>{
+    const updateImagesOrder = async (images) => {
         setImages(images);
     };
+
+    const setProductProp = (propName, value) => {
+        setProductProperties(prev => {
+            const newProductProps = { ...prev };
+            newProductProps[propName] = value;
+            return newProductProps;
+        })
+    }
+
+    const propertiesToFill = [];
+    if (categories.length > 0 && category) {
+        let catInfo = categories.find(({ _id }) => _id === category);
+        propertiesToFill.push(...catInfo?.properties);
+        while (catInfo?.parent?.id) {
+            const parentCat = categories.find(({ _id }) =>
+                _id === catInfo?.parent?._id);
+            propertiesToFill.push(...parentCat.properties);
+            catInfo = parentCat;
+        }
+    }
+
     return (
         <>
             <form onSubmit={saveProduct}>
@@ -74,22 +99,36 @@ const ProductForm = ({
                 <input value={title} onChange={ev => setTitle(ev.target.value)} type='text' placeholder='product name' />
                 <label>Category</label>
                 <select value={category}
-                    onChange={ev=>setCategory(ev.target.value)}>
+                    onChange={ev => setCategory(ev.target.value)}>
                     <option value={""}>Uncategorized</option>
-                    {categories.length>0 && categories.map((cat)=>(
+                    {categories.length > 0 && categories.map((cat) => (
                         <option key={cat._id} value={cat._id}>{cat.name}</option>
                     ))}
                 </select>
+                {propertiesToFill.length > 0 && propertiesToFill.map(p => (
+                    <div className=''>
+                        <label>{p.name}</label>
+                        <div>
+                            <select
+                                value={productProperties[p.name]}
+                                onChange={(ev) => setProductProp(p.name, ev.target.value)}>
+                                {p.values.map((v, i) => (
+                                    <option key={i} value={v}>{v}</option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>
+                ))}
                 <label>
                     Photos
                 </label>
                 <div className='mb-2 flex flex-wrap gap-2'>
                     <ReactSortable list={images} setList={updateImagesOrder} className='flex flex-wrap gap-1'>
-                    {!!images?.length && images.map(link =>(
-                        <div key={link} className='h-24'>
-                            <img src={link} alt="" className='rounded-lg'/>
-                        </div>
-                    ))}
+                        {!!images?.length && images.map(link => (
+                            <div key={link} className='h-24'>
+                                <img src={link} alt="" className='rounded-lg' />
+                            </div>
+                        ))}
                     </ReactSortable>
                     {isUploading && (
                         <div className='h-24 p-1 bg-gray-200 flex
@@ -97,14 +136,12 @@ const ProductForm = ({
                             <Spinner />
                         </div>
                     )}
-                    <label className='w-24 h-24 cursor-pointer text-center
-                    flex items-center justify-center text-sm gap-1
-                    text-gray-500 rounded-lg bg-gray-200'>
+                    <label className='w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1 text-primary rounded-sm bg-white shadow-sm border border-primary'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
                         </svg>
                         <div>
-                        Upload
+                            Add image
                         </div>
                         <input type="file" onChange={uploadImages} className='hidden' />
                     </label>
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -2,10 +2,15 @@ import multiparty from 'multiparty';
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import fs from 'fs';
 import mime from 'mime-types';
+import { mongooseConnect } from '@/lib/mongoose';
+import { isAdminRequest } from './auth/[...nextauth]';
 
 const bucketName = 'n-ecommerce';
 
 export default async function handle(req,res) {
+    await mongooseConnect();
+    await isAdminRequest(req,res);
+
     const form =new multiparty.Form();
     const {fileds, files} = await new Promise((resolve,reject) => {
         form.parse(req, (err, fields, files)=>{
