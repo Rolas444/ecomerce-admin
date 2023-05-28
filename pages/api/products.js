3aaed68e0a1125cd2d4b188cc2bd6cf48af64aad
DESKTOP-JBI6BJK\Hrodland <7200ws@gmail.com>
2023-05-27 19:08:27 -0500

    fix bugs product

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,10 +1,12 @@
 import { Product } from "@/models/Products";
 import { mongooseConnect } from "@/lib/mongoose";
+import { isAdminRequest } from "./auth/[...nextauth]";
 
 export default async function handle(req, res){
     const { method } = req;
 
     await mongooseConnect();
+    await isAdminRequest(req,res);
         
     if (method === 'GET'){
         if(req.query?.id){
